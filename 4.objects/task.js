3353c8ce64c1dd7f69c644f7e133260cb385eef2
Artem <s-art7@bk.ru>
2023-09-29 16:57:52 +0300

    Make the fourth task

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,36 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+    Student.prototype.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+    if(Array.isArray(this.marks)){
+        for(let elements of marksToAdd){
+            this.marks.push(elements);
+        }
+    }    
 }
 
 Student.prototype.getAverage = function () {
-  
+    let average = 0;
+    let sum = 0;
+    if(Array.isArray(this.marks) && this.marks.length > 0){
+        for (let elements of this.marks){
+            sum += elements;
+        }
+        average = sum / this.marks.length;
+    }
+    return average;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.marks;
+  delete Student.prototype.subject;
+  Student.prototype.excluded = reason;
 }
