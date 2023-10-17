57d0debc73169068733d9fb2219de78c30ffb03a
Artem <s-art7@bk.ru>
2023-10-17 11:14:14 +0300

    Make the sixth task

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,55 @@
-﻿
+﻿//task 1
+function parseCount(number) {
+    let result = Number.parseFloat(number);
+    if (isNaN(result)) {
+        throw new Error("Невалидное значение");
+    } else {
+        return result;
+    }
+}
+
+function validateCount(number) {
+    try {
+        return parseCount(number);
+    } catch (error) {
+        return error;
+    }
+}
+
+//task 2
+class Triangle {
+    constructor (side_A, side_B, side_C) {
+        if(side_A + side_B < side_C || side_A + side_C < side_B || side_B + side_C < side_A) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+        this.side_A = side_A;
+        this.side_B = side_B;
+        this.side_C = side_C;
+    }
+
+    get perimeter() {
+        return this.side_A + this.side_B + this.side_C;
+    }
+
+    get area() {
+        const halfOfPerimeter = this.perimeter / 2;
+        return +Math.sqrt(halfOfPerimeter*(halfOfPerimeter - this.side_A) * (halfOfPerimeter - this.side_B) * (halfOfPerimeter - this.side_C)).toFixed(3);
+    } 
+}
+
+function getTriangle(side_A, side_B, side_C) {
+    let errorMessage = "Ошибка! Треугольник не существует";
+    try {
+        let _triangle = new Triangle (side_A, side_B, side_C);
+        return _triangle;
+    } catch (error) {
+        return {
+            get perimeter() {
+                return errorMessage;
+            },
+            get area() {
+                return errorMessage;
+            }
+        }
+    }    
+}
