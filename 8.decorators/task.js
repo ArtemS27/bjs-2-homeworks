f2ecbe0761a831d5336f1a6caba08213cf456f2c
Artem <s-art7@bk.ru>
2023-10-30 11:54:09 +0200

    Make the eighth task

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,44 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
+    const cashe = [];
   
+    function wrapper(...args) {
+        const hash = md5(args);
+        let objectInCashe = cashe.find((item) => item.hash === hash);
+        if (objectInCashe) {
+            console.log("Из кеша: " + objectInCashe.result);
+            return "Из кеша: " + objectInCashe.result;
+        }
+        let result = func(...args);
+        cashe.push({hash, result});
+        if (cashe.length > 5) {
+            cashe.shift();
+        }
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result; 
+    }
+    return wrapper;
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+  let timeoutId = null;
+  function wrapper(...args) {
+    wrapper.allCount++;
+    
+    if (timeoutId) {
+        clearTimeout(timeoutId);
+    } else {
+        func(args);
+        wrapper.count++;
+    }
+    timeoutId = setTimeout(() => {
+        func(args);
+        wrapper.count++;
+    }, delay);
+  }
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+
+  return wrapper;
 }
